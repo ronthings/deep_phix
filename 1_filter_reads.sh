1f18b65b18bd90eff7422f6ac88da528bdb7774a
Ben Dickins <ben@Benjamins-Mac-mini.local>
2018-03-08 22:45:20 +0000

    Refresh cutadapt, remove sickle, long-form options
    Code refresh

--- 1_filter_reads.sh
+++ 1_filter_reads.sh
@@ -11,6 +11,14 @@ It will create the TMP/ and TRIM/ folders if they do not already exist.
 EOF
 }
 
+# determine how many CPUs on the current machine
+let NUMCPUS=$(sysctl -n hw.ncpu)
+# let NUMCPUS=$(cat /proc/cpuinfo | grep processor | wc -l)-1
+
+# adapter sequence for Nextera and Nextera XT: CTGTCTCTTATA[CACATCT]
+# this is reverse complement of last 19 nt in read 1 and read 2 primers
+adapter="CTGTCTCTTATA"
+
 # make the required output directories
 for d in "TMP" "TRIM" ; do
   if [ ! -d $d ] ; then
@@ -36,17 +44,20 @@ for ((i=0; i<=${#reads1[@]}-1; i++)); do # i from zero to one minus length of ar
   rvsrds="${reads2[$i]}" # e.g. "A_S1_L001_R2_001.fastq.gz"
   id="${fwdrds%%_*}" # greedy remove _ from right e.g. "A"
 
-  # cutadapt processes PE reads simultaneously: first filter against Q10, 5' (g/G) & 3' (a/A) search, discard trimmed reads
-  cutadapt -q 10,10 -g GGGCTCGG -a GACGCTGC -G GCAGCGTC -A CCGAGCCC --discard-trimmed \
-  -o TMP/${id}_filtered_R1.fastq.gz -p TMP/${id}_filtered_R2.fastq.gz \
+  # cutadapt processes PE reads simultaneously
+  # first: adaptive filter against Q30 (5' trim), and against Q30 (3' trim)
+  # default is quality-base=33 (http://drive5.com/usearch/manual/quality_score.html)
+  # second: 3' (a/A) adapter search, a (forward) & A (reverse) (-A important to avoid legacy mode)
+  # error-rate 0.1*12=1 mismatch allowed, min overlap length = 3 nts
+  # third: trim Ns
+  # forth: discard read pairs in which at least one member is <20 nts
+  # write files to TRIM directory
+  cutadapt --quality-cutoff 30,30 \
+  -a ${adapter} -A ${adapter} --error-rate=0.1 --overlap=3 \
+  --trim-n --pair-filter=any --minimum-length=20 --cores=$NUMCPUS \
+  -o TRIM/${id}_filtered_R1.fastq.gz -p TRIM/${id}_filtered_R2.fastq.gz \
   FASTQ/${fwdrds} FASTQ/${rvsrds}
 
-  # sickle: adaptively trim filtered reads to Q30, discard reads <20 bases, write paired + singletons to TRIM/
-  sickle pe -t sanger -l 20 -n -g -f TMP/${id}_filtered_R1.fastq.gz -r TMP/${id}_filtered_R2.fastq.gz \
-  -o TRIM/${id}_trimmed_R1.fastq.gz -p TRIM/${id}_trimmed_R2.fastq.gz -s TRIM/${id}_singles.fastq.gz
-
-  # cleanup filtered reads
-  rm TMP/${id}_filtered_R*.fastq.gz
 done
 
 echo [`date +"%Y-%m-%d %H:%M:%S"`] "#> DONE."
