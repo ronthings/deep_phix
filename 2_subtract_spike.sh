42ab3df7706ba67162457dd1720801cc0f690b13
Ben Dickins <ben.dickins@cantab.net>
2018-04-28 16:28:33 +0100

    remove single end workflow

--- 2_subtract_spike.sh
+++ 2_subtract_spike.sh
@@ -30,25 +30,16 @@ for ((i=0; i<=${#reads1[@]}-1; i++)); do
   fwdrds="${reads1[$i]}" # e.g. "A_trimmed_R1.fastq.gz"
   rvsrds="${reads2[$i]}" # e.g. "A_trimmed_R2.fastq.gz"
   id="${fwdrds%%_*}" # greedy remove _* from right e.g. "A"
-  sgls="${id}_singles.fastq.gz" # e.g. "A_singles.fastq.gz"
-
-  # mapping SE reads (no read groups), need to make {1-2686,1-250} genome
-  bwa mem -t 4 FASTA/pUC18_L09136.fasta TRIM/${sgls} > TMP/${id}_greedymapped_SE.sam
-
-  # select UNMAPPED reads (f=flag present, 4=unmapped) and cleanup
-  samtools view -b -f 4 -o TMP/${id}_unmapped_SE.bam TMP/${id}_greedymapped_SE.sam
-  rm TMP/${id}_greedymapped_SE.sam # remove SAM
 
   # mapping PE reads, can I make these map in a single-end mode??
   bwa mem -t 4 FASTA/pUC18_L09136.fasta TRIM/${fwdrds} TRIM/${rvsrds} > TMP/${id}_greedymapped_PE.sam
 
-  # select UNMAPPED reads here too, then sort by read name (-n, because we need to emit paired end files next) and cleanup
+  # select UNMAPPED reads (f=flag present, 4=unmapped), sort by read name (-n, because we need to emit paired end files next) and cleanup
   # explanation: | is pipe and - is reference to intermediate output)
   samtools view -O SAM -h -f 4 TMP/${id}_greedymapped_PE.sam | samtools sort -O BAM -n -o TMP/${id}_unmapped_PE.bam -
   rm TMP/${id}_greedymapped_PE.sam # remove SAM
 
-  # convert SE and PE BAM files to FASTQ (for PE singletons will be discarded by bedtools, which is conservative), cleanup
-  bedtools bamtofastq -i TMP/${id}_unmapped_SE.bam -fq TRIM/${id}_unmapped_SE.fastq
+  # convert PE BAM files to FASTQ (for PE singletons are by bedtools, which is conservative), cleanup
   bedtools bamtofastq -i TMP/${id}_unmapped_PE.bam -fq TRIM/${id}_unmapped_R1.fastq -fq2 TRIM/${id}_unmapped_R2.fastq
   rm TMP/${id}_unmapped_*.bam # remove BAMs
   gzip TRIM/${id}_unmapped_*.fastq # zip FASTQs for space
